7bd6f108141c35dd64221606ac2af4c1ca2408ec
TrinhHoangKhang <trinhhoangkhangvl@gmail.com>
2024-06-14 19:59:37 +0700

    Update test_activation_function.py

--- Homework/Week1/test_activation_function.py
+++ Homework/Week1/test_activation_function.py
@@ -5,7 +5,7 @@ import sys
 import os
 from io import StringIO
 
-# sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '../../../Homework/Week1')))
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '../../Homework/Week1')))
 import activation_function as af
 
 def test_is_number():
